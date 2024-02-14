d909274aaeaa2989591f2c6ab502aa4b6458f1da
Ashna Nadar <42215047+AshnaNadar@users.noreply.github.com>
2024-02-14 16:40:52 -0500

    Created navigation controller and bottom menu bar with functionality for Login and Today's Workout / Home pages.

--- clean-android-ui/app/src/main/java/org/example/userinterface/UserView.kt
+++ clean-android-ui/app/src/main/java/org/example/userinterface/UserView.kt
@@ -1,56 +1,82 @@
 package org.example.userinterface
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import org.example.controller.UserController
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 
-enum class ViewEvent {
-    FirstNameEvent,
-    LastNameEvent,
-    UppercaseEvent,
-    LowercaseEvent,
-    ResetEvent
-}
-
+/*
+middle section of scaffold for fragments
+ */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun UserView(userViewModel: UserViewModel, userController: UserController) {
-    val viewModel by remember { mutableStateOf(userViewModel) }
-    val controller by remember { mutableStateOf(userController) }
-
-    Column(verticalArrangement = Arrangement.SpaceEvenly) {
-        TextField(
-            viewModel.firstname.value,
-            label = {Text("First Name: ")},
-            onValueChange = { controller.invoke(ViewEvent.FirstNameEvent, it) })
-        TextField(
-            viewModel.lastname.value,
-            label = {Text("Last Name: ")},
-            onValueChange = { controller.invoke(ViewEvent.LastNameEvent, it) })
-
-        Row (horizontalArrangement = Arrangement.SpaceEvenly) {
-            Button(onClick = {
-                controller.invoke(ViewEvent.UppercaseEvent, null)
-            }) {
-                Text("UPPER")
-            }
-            Button(onClick = {
-                controller.invoke(ViewEvent.LowercaseEvent, null)
-            }) {
-                Text("lower")
-            }
-            Button(onClick = {
-                controller.invoke(ViewEvent.ResetEvent, null)
-            }) {
-                Text("Reset")
-            }
+fun UserView(
+    //userViewModel: UserViewModel,
+    //userController: UserController,
+    onEquipmentButtonClicked: () -> Unit = {},
+    onTodayButtonClicked: () -> Unit = {},
+    onSavedButtonClicked: () -> Unit = {},
+    onSettingsButtonClicked: () -> Unit = {},
+    modifier: Modifier = Modifier
+) {
+    //val viewModel by remember { mutableStateOf(userViewModel) }
+    //val controller by remember { mutableStateOf(userController) }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Home Screen")
+                }
+            )
+        },
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text =
+                """
+                    Fragments go here.
+                """.trimIndent(),
+            )
         }
     }
 }
+
