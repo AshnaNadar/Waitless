b479c24339d9d3adc2ff5c319894e351d692a03a
Michael Mavely <mmavely@uwaterloo.ca>
2024-02-15 17:10:54 -0500

    fixed login fields

--- clean-android-ui/app/src/main/java/org/example/userinterface/UserViewModel.kt
+++ clean-android-ui/app/src/main/java/org/example/userinterface/UserViewModel.kt
@@ -4,15 +4,15 @@ import androidx.compose.runtime.mutableStateOf
 import org.example.model.UserModel
 
 class UserViewModel(val model: UserModel) : ISubscriber {
-    var firstname = mutableStateOf("")
-    var lastname = mutableStateOf("")
+    var username = mutableStateOf("")
+    var password = mutableStateOf("")
 
     init {
         model.subscribe(this)
     }
 
     override fun update() {
-        firstname.value = model.firstname
-        lastname.value = model.lastname
+        username.value = model.username
+        password.value = model.password
     }
 }
