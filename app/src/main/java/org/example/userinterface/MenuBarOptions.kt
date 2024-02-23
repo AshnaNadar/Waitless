58a24190a891588a95d0c0944c73182835894af6
hduraiba <hduraiba@uwaterloo.ca>
2024-02-23 17:32:54 -0500

    Menu bar UI

--- app/src/main/java/org/example/userinterface/MenuBarOptions.kt
+++ app/src/main/java/org/example/userinterface/MenuBarOptions.kt
@@ -12,47 +12,40 @@ sealed class MenuBarOptions (
     val route: String,
     val title: String,
     val icon: ImageVector,
-    val icon_focused: ImageVector
 ) {
     data object Login: MenuBarOptions(
         route = "login",
         title = "Login",
         icon = Icons.Default.ExitToApp,
-        icon_focused = Icons.Default.ExitToApp
     )
 
     data object EquipmentInfo: MenuBarOptions(
         route = "equipment/info",
         title = "Equipment Info",
         icon = Icons.Default.ExitToApp,
-        icon_focused = Icons.Default.ExitToApp
     )
     data object Home: MenuBarOptions(
         route = "home",
         title = "Home",
         icon = Icons.Default.Home,
-        icon_focused = Icons.Default.Home
     )
 
     data object Equipment: MenuBarOptions(
         route = "equipment",
         title = "Equipment",
         icon = Icons.Default.List,
-        icon_focused = Icons.Default.Home
     )
 
     data object Saved: MenuBarOptions(
         route = "saved",
         title = "Saved",
         icon = Icons.Default.Favorite,
-        icon_focused = Icons.Default.Home
     )
 
     data object Settings: MenuBarOptions(
         route = "settings",
         title = "Settings",
         icon = Icons.Default.Settings,
-        icon_focused = Icons.Default.Home
     )
 }
 
