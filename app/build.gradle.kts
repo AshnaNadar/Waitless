1b3e56c3f7a3672c48ee6ef33b20c29d9f0c6799
mgmavely <mgmavely@gmail.com>
2024-03-07 20:28:42 -0500

    Emulator working with login verification

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
+val ktorVersion: String by project
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.0"
 }
 
 android {
@@ -49,7 +52,7 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
@@ -70,4 +73,14 @@ dependencies {
 
     // Navigation Compose
     implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    // Ktor
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-json:$ktorVersion")
+    implementation("io.ktor:ktor-client-serialization:$ktorVersion")
+    implementation("io.ktor:ktor-client-logging:$ktorVersion")
+    implementation("io.ktor:ktor-client-resources:$ktorVersion")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
 }
--- app/src/main/java/org/example/userinterface/Login/LoginView.kt
+++ app/src/main/java/org/example/userinterface/Login/LoginView.kt
@@ -1,45 +1,58 @@
 package org.example.userinterface.Login
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import org.example.controller.UserController
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import io.ktor.client.*
+import io.ktor.client.call.body
+import io.ktor.client.request.*
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.util.InternalAPI
+import kotlinx.coroutines.launch
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+@Serializable
+data class LoginRequest(val email: String, val password: String)
+
 @Preview
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, InternalAPI::class)
 @Composable
 fun LoginView(
     onLoginButtonClicked: () -> Unit = {}
 ) {
-    var username by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("")}
+    var showErrorMessage by remember { mutableStateOf(false) }
+    val coroutineScope = rememberCoroutineScope()
 
     Scaffold(
         topBar = {
@@ -52,9 +65,9 @@ fun LoginView(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.SpaceEvenly) {
             TextField(
-                username,
+                email,
                 label = {Text("Username: ")},
-                onValueChange = { username = it })
+                onValueChange = { email = it })
             TextField(
                 password,
                 label = {Text("Password: ")},
@@ -62,11 +75,50 @@ fun LoginView(
                 visualTransformation =  PasswordVisualTransformation(),
                 keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password))
 
+            if (showErrorMessage) {
+                Text(
+                    text = "Invalid Username/Password",
+                    color = Color.Red,
+                    modifier = Modifier.padding(vertical = 8.dp),
+                    style = TextStyle(fontSize = 16.sp)
+                )
+            }
+
             Row (horizontalArrangement = Arrangement.SpaceEvenly) {
-                Button(onClick = onLoginButtonClicked) {
+                Button(onClick = {
+                    coroutineScope.launch {
+                        val httpClient = HttpClient()
+                        try {
+                            Log.d("TRY Block:", "${email}, ${password}")
+                            val response: HttpResponse = httpClient.post("http://10.0.2.2:8080/auth/signin") {
+                                contentType(ContentType.Application.Json)
+                                body = Json.encodeToString(LoginRequest(email, password))
+                            }
+                            when (response.status.value) {
+                                in 200..299 -> {
+                                    // Successful login
+                                    val responseBody: String = response.body()
+                                    Log.d("ResponseLogin:", responseBody)
+                                    onLoginButtonClicked()
+                                }
+                                else -> {
+                                    // Other error occurred
+                                    Log.d("ResponseLogin:", "Unexpected response code: ${response.status.value}")
+                                    showErrorMessage = true
+                                }
+                            }
+                        } catch (e: Exception) {
+                            println("Error: ${e.message}")
+                        } finally {
+                            httpClient.close()
+                        }
+                    }
+                }) {
                     Text("Log In")
                 }
             }
         }
     }
 }
+
+
